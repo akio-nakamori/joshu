57a76de47d28797e1a78c033cac4bf95ec2c013c
bigretromike <bigretromike@users.noreply.github.com>
2017-02-27 18:33:10 +0100

    cleanup;

--- app.py
+++ app.py
@@ -1,43 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import adbb
 import json
 import logging
 import os
+import sys
 
-config = json.load(open('./config.json'))
 
-sql = "mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8mb4".format(config.get('db_user'), config.get('db_pass'), config.get('db_host'), config.get('db_port'), config.get('db_name'))
+debug = True
 
-# custom logger
-if os.name != 'posix':
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.INFO)
 
-    logger.setLevel(logging.DEBUG)
-    lh = logging.StreamHandler()
-    lh.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(filename)s:%(lineno)d - %(message)s'))
-    logger.addHandler(lh)
+def init():
+    """
+    Init all needed things to make joshu up and running
+    :return:
+    """
+    config = json.load(open('./config.json'))
+    sql = "mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8mb4".format(config.get('db_user'), config.get('db_pass'), config.get('db_host'), config.get('db_port'), config.get('db_name'))
 
-    lh = logging.handlers.SysLogHandler(address=('{}'.format(config.get('syslog_host')), 514))
-    lh.setFormatter(logging.Formatter('joshu %(filename)s/%(funcName)s:%(lineno)d - %(message)s'))
-    logger.addHandler(lh)
+    # custom logger
+    if os.name != 'posix':
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logging.INFO)
 
-    # initialize backend
-    adbb.init(config.get('user'), config.get('pass'), sql, debug=True, logger=logger)
-else:
-    adbb.init(config.get('user'), config.get('pass'), sql, debug=True)
+        logger.setLevel(logging.DEBUG)
+        lh = logging.StreamHandler()
+        lh.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(filename)s:%(lineno)d - %(message)s'))
+        logger.addHandler(lh)
 
-anime = adbb.Anime(6187)
+        lh = logging.handlers.SysLogHandler(address=(config.get('syslog_host'), 514))
+        lh.setFormatter(logging.Formatter('joshu %(filename)s/%(funcName)s:%(lineno)d - %(message)s'))
+        logger.addHandler(lh)
 
-# this will print "Kemono no Souja Erin has 50 episodes and is a TV Series"
-print("{} has {} episodes and is a {}".format(anime.title, anime.nr_of_episodes, anime.type))
+        # initialize backend
+        adbb.init(config.get('user'), config.get('pass'), sql, debug=True, logger=logger)
+    else:
+        adbb.init(config.get('user'), config.get('pass'), sql, debug=True)
 
-# Episode object can be created either using anime+episode number or the anidb eid
-# anime can be either aid, title or Anime object
-#episode = adbb.Episode(eid=96461)
 
-# this will print "'Kemono no Souja Erin' episode 5 has title 'Erin and the Egg Thieves'"
-#print("'{}' episode {} has title '{}'".format(episode.anime.title, episode.episode_number, episode.title_eng))
+def command_scan(path):
+    """
+    Scan and add files from given path
+    :param path: path to scan
+    :return:
+    """
+    root = unicode(path)
+    if os.path.exists(path):
+        file_count = 0
+        for dir_path, dir_names, file_names in os.walk(root):
+            for file_name in file_names:
+                file_count += 1
+                log(str(file_count) + '/' + str(len(file_names)))
+                _file = adbb.File(path=os.path.join(dir_path, file_name))
+                print("'{}' contains episode {} of '{}'. Mylist state is '{}'".format(_file.path, _file.episode.episode_number, _file.anime.title, _file.mylist_state))
+    else:
+        print("Path not found.")
 
-# note that most of the time this will work even if we use a file that is not in the anidb database
-# will print "'<path>' contains episode 5 of 'Kemono no Souja Erin'. Mylist state is 'on hdd'"
-#print("'{}' contains episode {} of '{}'. Mylist state is '{}'".format(file.path, file.episode.episode_number, file.anime.title, file.mylist_state))
+
+def log(message):
+    """
+    simple debug log function
+    :param message: anything
+    :return: print str of anything
+    """
+    if debug:
+        print(message)
+
+
+if __name__ == '__main__':
+    if (len(sys.argv)) < 2:
+        print("No command found.")
+    else:
+        if sys.argv[1] == "scan":
+            path_to_scan = sys.argv[2]
+            if path_to_scan != "":
+                init()
+                command_scan(path_to_scan)
+            else:
+                print("No path was given.")
+    sys.exit()
